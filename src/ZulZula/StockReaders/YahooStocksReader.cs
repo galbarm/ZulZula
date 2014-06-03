e0c7e26c9c6b8ea28ad5f43e8e7a45e68ca6ca07
Ilan Schwarts <ilan@softimize.co.il>
2014-06-03 15:18:10 +0300

    No comments

--- src/ZulZula/StockReaders/YahooStocksReader.cs
+++ src/ZulZula/StockReaders/YahooStocksReader.cs
@@ -7,6 +7,7 @@ namespace ZulZula
 {
     public class YahooStocksReader : IStockReader
     {
+
         public Stock GetStock(string fullpath)
         {
             var rates = new List<StockEntry>();
