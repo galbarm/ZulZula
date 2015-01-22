afc296de21a1c1e4e83f115e5d7257e8da086809
galbarm <97ebV8lGOwKebQ*E>
2015-01-22 21:32:03 +0200

    added IStockEntry

---
+++ src/ZulZula/Stocks/IStockEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZulZula.Stocks
+{
+    public interface IStockEntry
+    {
+        DateTime Date { get; }
+        double Open { get; }
+        double High { get; }
+        double Low { get; }
+        double Close { get; }
+        double Volume { get; }
+        double AdjClose { get; }
+
+        double Adj { get; }
+    }
+}
